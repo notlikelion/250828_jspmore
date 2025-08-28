2a08814cddcb908b46d3e6c28369b7e3294229c5
notlikelion <notlikelion@gmail.com>
2025-08-28 13:11:51 +0900

    Initial commit

---
+++ src/main/java/com/example/jspmore/JspmoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspmoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jspmore/JspmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
